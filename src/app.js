f5e71b17b790b96725a56c8730f1e0574e25cca6
seabasshoang <sylviahoang@hotmail.co.uk>
2019-04-29 22:58:58 +0100

    set up express handlebars and server

---
+++ src/app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const handlebars = require('express-handlebars');
+const bodyParser = require('body-parser');
+const controllers = require('./controllers/index');
+// const helpers = require('./views/helpers/index');
+
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+app.engine('hbs',
+    handlebars({
+        extname: 'hbs',
+        layoutsDir: path.join(__dirname, 'views', 'layouts'),
+        partialsDir: path.join(__dirname, 'views', 'partials'),
+        defaultLayout: 'main',
+        // helpers,
+    })
+    );
+
+app.disable('x-powered-by');
+app.set('port', process.env.PORT || 3000);
+app.use(favicon(path.join(__dirname, '..', 'public', 'assets', 'favicon.ico')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, '..', 'public')));
+app.use(controllers);
+
+module.exports = app;
